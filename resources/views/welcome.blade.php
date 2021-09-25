76a0d64ec4722bde25b064855d048b39523c413b
kensuke <35k.0101@gmail.com>
2021-09-25 20:56:47 +0900

    add: vue ui

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
         <title>Laravel</title>
 
         <!-- Fonts -->
@@ -78,23 +79,12 @@
                     @endauth
                 </div>
             @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+            <div id="app">
+            <!-- デフォルトだとこの中ではvue.jsが有効 -->
+            <!-- example-component はLaravelに入っているサンプルのコンポーネント -->
+                <example-component></example-component>
             </div>
         </div>
+        <script src=" {{ mix('js/app.js') }} "></script>
     </body>
 </html>
